ff1bcb60b1bc4ba9dce5cf3af1b3f62cada62e4a
florianboergel <florian.boergel@io-warnemuende.de>
2022-02-06 15:04:30 +0100

    doku

--- install.R
+++ install.R
@@ -3,3 +3,15 @@ install.packages("rmarkdown")
 install.packages("httr")
 install.packages("shinydashboard")
 install.packages('leaflet')
+
+install.packages("reticulate") # python support in RMarkdown
+install.packages("ggplot2") # for plotting
+install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+
+### install bioconductor packages
+# install.packages("BiocManager")
+# BiocManager::install("package")
+
+### install GitHub packages (tag = commit, branch or release tag)
+# install.packages("devtools")
+# devtools::install_github("user/repo", ref = "tag")
