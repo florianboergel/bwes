705937d35be1bb33d0524f790c7f39d90e9be8cb
florianboergel <florianboergel@io-warnemuende.de>
2022-02-16 14:04:10 +0100

    Remove versioning error

--- binder/install.R
+++ binder/install.R
@@ -6,14 +6,9 @@ install.packages("ggplot2") # for plotting
 install.packages("RNetCDF")
 install.packages("fields")
 install.packages("WaveletComp")
-
-install.packages('IRkernel')
-IRkernel::installspec()
-
 ### install bioconductor packages
 # install.packages("BiocManager")
 # BiocManager::install("package")
-
 ### install GitHub packages (tag = commit, branch or release tag)
 # install.packages("devtools")
 # devtools::install_github("user/repo", ref = "tag")
